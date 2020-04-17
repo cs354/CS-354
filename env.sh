0b019d8b508a17a2ba1d7b9568bd2fd482fb12e9
Aaron Nelson <aaron@robertnelson.xyz>
2020-04-17 15:15:17 -0400

    hide error message when executing for first time

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-if ! docker start -i `docker ps -qaf ancestor=cs354/student-env`; then
+if ! docker start -i `docker ps -qaf ancestor=cs354/student-env` 2> /dev/null; then
   docker run -it --network host cs354/student-env
 fi
