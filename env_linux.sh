96e77a18697bfdf8963fa4a7fe2645c3bb72b6e7
Aaron Nelson <aaron@robertnelson.xyz>
2020-04-19 11:21:25 -0400

    add root check and improve error msg

--- env_linux.sh
+++ env_linux.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 IMG_NAME=cs354/student-env
+
+if [[ $EUID != 0 ]]
+then
+  echo "This script must be run as root"
+  exit
+fi
+
 if [[ $(docker ps -qf ancestor\=$IMG_NAME)  ]]
 then
   echo "The environment is already running. If you can't exit it normally, run:"
   echo "sudo docker stop " `docker ps -qf ancestor=$IMG_NAME`
+  echo "then run this script again."
   exit
 fi
 
 if ! docker start -i `docker ps -qaf ancestor=$IMG_NAME` 2> /dev/null; then
-  docker run -it --network host cs354/student-env
+  docker run -it --network host $IMG_NAME
 fi
