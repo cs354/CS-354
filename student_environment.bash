6e52d0b044941512effe75b4dbf654af446832df
n8ta <n8@u.northwestern.edu>
2020-05-13 15:45:26 -0500

    allow multiple attachments to the stu container

--- student_environment.bash
+++ student_environment.bash
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 IMG_NAME=cs354/student-env:latest
 
+readme() {
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+  echo "   "
+  echo "   CS-354 Student Env"
+  echo ""
+  echo "   This container will be reused across many of the labs and "
+  echo "   projects. You can store files in this container and they will "
+  echo "   not be removed unless you delete the container manually."
+  echo "   This is not true of the other containers used in the class"
+  echo " "
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+}
 
 ID=local
 if [ $(hostname) == vicious ]
@@ -19,23 +31,29 @@ NETWORK_NAME=cs354-$ID
 
 if [[ $(docker ps -qf name=$CTR_NAME) ]]
 then
-  echo "The environment is already running. If you can't exit it normally, run:"
-  echo "docker stop " `docker ps -qf name=$CTR_NAME`
-  echo "then run this script again."
-  exit
+  echo "The environment is already running. You can start another session or stop the container."
+  echo "This will not create a new container just a new terminal to access it."
+  echo "Type 'stop' or 'new'"
+  read;
+  reply=${REPLY}
+  if [ ${reply} = stop ]
+  then
+    echo "Stopping"
+    docker stop `docker ps -qf name=$CTR_NAME`
+    exit
+  elif [ ${reply} = new ]
+  then
+    echo "New session..."
+    readme
+    docker exec -it `docker ps -qf name=$CTR_NAME` /bin/bash
+    exit
+  else
+    echo "You must type stop or new... Exitting."
+    exit
+  fi
 fi
 
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "   "
-echo "   CS-354 Student Env"
-echo ""
-echo "   This container will be reused across many of the labs and "
-echo "   projects. You can store files in this container and they will "
-echo "   not be removed unless you delete the container manually."
-echo "   This is not true of the other containers used in the class"
-echo " "
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-
+readme
 
 docker network create $NETWORK_NAME > /dev/null 2>&1
 if ! docker start -i `docker ps -qaf name=$CTR_NAME` 2> /dev/null; then
