61bcb6ba6ccfa470675d323376759940d2417843
Isaac Jin <whale3ye@gmail.com>
2025-02-19 00:22:47 -0600

    fix submission script for web attack project

--- mnt/projects/web-attacks/util/submit.py
+++ mnt/projects/web-attacks/util/submit.py
@@ -5,52 +5,54 @@ import sys
 import requests
 import warnings
 
-SITE = 'hamsa.cs.northwestern.edu'
-LOGIN = 'https://hamsa.cs.northwestern.edu/login/'
+SITE = "spike.cs.northwestern.edu"
+LOGIN = "https://spike.cs.northwestern.edu/login/"
 
 
 def submit(project, file):
-    username = input('Username: ')
-    password = getpass('Password: ')
+    username = input("Username: ")
+    password = getpass("Password: ")
 
-    payload = {'username': username, 'password': password}
+    payload = {"username": username, "password": password}
     session = requests.Session()
     session.post(LOGIN, data=payload, verify=False)
-    url = 'http://%s/projects/%i/submit/' % (SITE, project)
-    files = {'file': open(file, 'rb')}
+    url = "https://%s/projects/%i/submit/" % (SITE, project)
+    files = {"file": open(file, "rb")}
     response = session.post(url, files=files, verify=False)
     try:
         submission = int(response.text)
     except ValueError:
-        sys.stderr.write('Invalid username and password\n')
+        sys.stderr.write("Invalid username and password\n")
         sys.exit(1)
 
     started = False
     count = -1
     while True:
-        response = session.get('http://%s/projects/status/%i/' % (SITE, submission))
+        response = session.get("https://%s/projects/status/%i/" % (SITE, submission))
         status = int(response.text)
         if status == 0:
             break
         elif status == 1:
             if not started:
                 started = True
-                if count > 0: sys.stderr.write('\n')
-                sys.stderr.write('Testing started')
+                if count > 0:
+                    sys.stderr.write("\n")
+                sys.stderr.write("Testing started")
         elif count <= 0:
-            if count == 0: sys.stderr.write('\n')
-            sys.stderr.write('At position %i in queue' % (status - 1))
+            if count == 0:
+                sys.stderr.write("\n")
+            sys.stderr.write("At position %i in queue" % (status - 1))
             count = 10
         count -= 1
-        sys.stderr.write('.')
+        sys.stderr.write(".")
         sleep(1)
 
-    sys.stderr.write('\n')
+    sys.stderr.write("\n")
 
-    response = session.get('http://%s/projects/result/%i/' % (SITE, submission))
+    response = session.get("https://%s/projects/result/%i/" % (SITE, submission))
     print(response.text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     warnings.simplefilter("ignore")
     submit(int(sys.argv[1]), sys.argv[2])
